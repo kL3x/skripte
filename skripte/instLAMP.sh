67f3a07096708c1090b44f285769b8afe40c5bc1
Daniel Meier <kL3x@users.noreply.github.com>
2015-01-28 13:53:58 +0100

    Update instLAMP.sh

--- skripte/instLAMP.sh
+++ skripte/instLAMP.sh
@@ -1,2 +1,2 @@
-pt-get install -y apache2 apache2-utils mysql-server libapache2-mod-auth-mysql php5-mysql php5 php5-mysql php-pear php5-gd  php5-mcrypt php5-curl phpmyadmin mytop iotop iftop goaccess bmon apachetop
+apt-get install -y apache2 apache2-utils mysql-server libapache2-mod-auth-mysql php5-mysql php5 php5-mysql php-pear php5-gd  php5-mcrypt php5-curl phpmyadmin mytop iotop iftop goaccess bmon apachetop
 
